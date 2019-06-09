cf1d44b80acd385547490028a9db02376902542b
Justin Im <20imj68@myahs.org>
2019-06-09 00:45:49 -0700

    Despite lack of phones and lack of OTG, easy code is done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Trebuchet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Trebuchet.java
@@ -2,15 +2,18 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Trebuchet {
     DcMotor catapult;
     DcMotor rack;
+    Servo servo;
     public static final double TICKS_PER_ROTATION = 1440;
     DcMotor[] motors;
-    public Trebuchet(DcMotor catapult, DcMotor rack){
+    public Trebuchet(DcMotor catapult, DcMotor rack, Servo s){
         this.catapult = catapult;
         this.rack = rack;
+        this.servo = s;
         catapult.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
@@ -21,6 +24,9 @@ public class Trebuchet {
 //        }
         catapult.setPower(gp.left_trigger);
         rack.setPower(gp.right_trigger);
+        if (gp.a) {
+
+        }
 
     }
 
@@ -34,6 +40,21 @@ public class Trebuchet {
 
     public void resetEncoders(){
         catapult.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    public void changePos(){
+        double pos = servo.getPosition();
+        if (pos == 1.0){
+            turnServo(servo, 0.0);
+        }
+        else {
+            turnServo(servo, 1.0);
+        }
+    }
+    public void turnServo(Servo serv, double pos){
+        servo.setPosition(pos);
+        while (servo.getPosition() != pos) {
+            //wait
+        }
     }
 
     public void rest(){
